0e5e0335cfdcf8621276f3c5c6fc8c64bee6e061
ngoc.dang <ngoc.dang@whitecloudwt.com>
2026-07-01 14:18:42 +0700

    #

--- application/views/quotation-detail.php
+++ application/views/quotation-detail.php
@@ -1,69 +1,59 @@
-<!--Inner Banner-->
-<div class="aboutbanner innerbanner">
-	<div class="inner-breadcrumb">
-		<div class="container">
-			<div class="row align-items-center text-center">
-				<div class="col-md-12 col-12 ">
-					<h2 class="breadcrumb-title">Báo Giá</h2>
-					<nav aria-label="breadcrumb" class="page-breadcrumb">
-						<ol class="breadcrumb">
-							<li class="breadcrumb-item"><a href="<?php echo base_url() ?>">Trang chủ</a></li>
-							<li class="breadcrumb-item active" aria-current="page">Báo giá</li>
-						</ol>
-					</nav>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<!--/Inner Banner-->
-
-<!-- Blog -->
-<div class="bloglisting">
+<section class="blog-single padding-top-30 padding-bottom-60">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-9">
-				<div class="blogdetail-content" style="padding-top: 0;">
-					<h1 style="text-align: center; margin-bottom: 50px;"><?php echo $quotation['name'] ?></h1>
-					<?php echo $quotation['content'] ?>
-				</div>
+			<div class="col-md-9">
 
-			</div>
-			<div class="col-lg-3 theiaStickySidebar">
-				<div class="rightsidebar blogsidebar">
-					<div class="card">
-						<h4><img src="<?php echo base_url() ?>assets/listee/img/details-icon.svg" alt="details-icon"> Tìm kiếm </h4>
-						<div class="filter-content looking-input form-group">
-							<input type="text" class="form-control" placeholder="To Search type and hit enter">
+				<!-- Blog Post -->
+				<div class="blog-post">
+					<article>
+						<div>
+							<span>By: <strong>Administrator</strong></span>
+							<span><i class="fa fa-bookmark-o"></i> <?php echo date("M", strtotime($quotation['updated_at'])) ?> <?php echo date("d", strtotime($quotation['updated_at'])) ?>, <?php echo date("Y", strtotime($quotation['updated_at'])) ?></span>
 						</div>
+						<h5><?php echo $quotation['name'] ?></h5>
+					<div class="post-content" style="all: revert;">
+							<?php echo $quotation['content'] ?>
+						</div>
+					</article>
+				</div>
+			</div>
+
+			<!-- Side Bar -->
+			<div class="col-md-3">
+				<div class="shop-side-bar">
+
+					<!-- Search -->
+					<div class="search">
+						<form>
+							<label>
+								<input type="email" placeholder="Search here">
+							</label>
+							<button type="submit"><i class="fa fa-search"></i></button>
+						</form>
 					</div>
-					<div class="card">
-						<h4><img src="<?php echo base_url() ?>assets/listee/img/icons/icons-3.png" alt="details-icon" width="20px"> Báo giá liên quan </h4>
-						<ul class="blogcategories-list">
-							<?php foreach ($related_quotations as $index => $item) { ?>
-								<li> <a href="<?php echo base_url() . 'bao-gia/' . $item['name_unsigned'] ?>"> <?php echo $item['name'] ?> </a> </li>
-							<?php } ?>
+
+					<!-- Categories -->
+					<h6><img src="<?php echo base_url() ?>assets/listee/img/details-icon.svg" alt="details-icon"> Danh mục</h6>
+					<div class="checkbox checkbox-primary">
+						<ul>
+							<li> <a href="<?php echo base_url() . 'danh-muc-bao-gia/bao-gia-tam-compact' ?>"> Báo giá tấm Compact </a> </li>
+							<li> <a href="<?php echo base_url() . 'danh-muc-bao-gia/bao-gia-vach-ngan-ve-sinh' ?>"> Báo giá vách ngăn vệ sinh </a> </li>
+							<li> <a href="<?php echo base_url() . 'danh-muc-bao-gia/bao-gia-vach-ngan-di-dong' ?>"> Báo giá vách ngăn di động </a> </li>
 						</ul>
 					</div>
-					<div class="card">
-						<h4><img src="<?php echo base_url() ?>assets/listee/img/category-icon.svg" alt="details-icon"> Báo giá </h4>
-						<ul class="blogcategories-list">
-							<li> <a href="<?php echo base_url() . 'danh-muc-bao-gia/bao-gia-tam-compact'?>"> Báo giá tấm Compact </a> </li>
-							<li> <a href="<?php echo base_url() . 'danh-muc-bao-gia/bao-gia-vach-ngan-ve-sinh'?>"> Báo giá vách ngăn vệ sinh </a> </li>
-							<li> <a href="<?php echo base_url() . 'danh-muc-bao-gia/bao-gia-vach-ngan-di-dong'?>"> Báo giá vách ngăn di động </a> </li>
-						</ul>
-					</div>
-					<div class="card tags-widget">
-						<h4><i class="feather-tag"></i> Tags</h4>
-						<ul class="tags">
-							<?php foreach ($tags as $index => $item) { ?>
-								<li><?php echo $item ?></li>
-							<?php } ?>
-						</ul>
+
+					<!-- Recent Posts -->
+					<h6><img src="<?php echo base_url() ?>assets/listee/img/category-icon.svg" alt="details-icon"> Báo giá liên quan</h6>
+					<div class="recent-post">
+						<?php foreach ($related_quotations as $index => $item) { ?>
+							<div class="media">
+								<div class="media-left"> <a href="<?php echo base_url() . 'bao-gia/' . $item['name_unsigned'] ?>"><img class="img-responsive" src="<?php echo base_url() . POST_UPLOAD_PATH . $item['avatar'] ?>" alt=""> </a> </div>
+								<div class="media-body"> <a href="<?php echo base_url() . 'bao-gia/' . $item['name_unsigned'] ?>"><?php echo $item['name'] ?></a> <span><?php echo date("M", strtotime($item['updated_at'])) ?> <?php echo date("d", strtotime($item['updated_at'])) ?>, <?php echo date("Y", strtotime($item['updated_at'])) ?> </span></div>
+							</div>
+						<?php } ?>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
-<!-- /Blog -->
+</section>
