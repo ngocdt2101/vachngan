37640f5f63952eee7e2a64ac6dd638f5acc8cf14
ngoc.dang <ngoc.dang@whitecloudwt.com>
2026-07-11 00:06:51 +0700

    #

--- application/views/about.php
+++ application/views/about.php
@@ -1,6 +1,11 @@
-<section class="about-sec padding-top-60 padding-bottom-20">
+<section class="about-sec">
 	<div class="container">
-		<img src="<?php echo base_url() . POST_UPLOAD_PATH . $about['banner'] ?>" class="img-fluid" alt="">
+		<div class="section-heading text-center">
+			<h2>Giới Thiệu</h2>
+			<p>Khám phá câu chuyện, tầm nhìn và giá trị cốt lõi của công ty</p>
+			<hr>
+		</div>
+		<img src="<?php echo base_url() . POST_UPLOAD_PATH . $about['banner'] ?>" class="img-fluid" alt="About Banner">
 	</div>
 </section>
 
@@ -10,8 +15,8 @@
 	<div class="container">
 		<div class="about-listee">
 			<div class="about-info">
-				<h4> <span>Về </span> Chúng Tôi</h4>
-				<p> <?php echo $about['description'] ?></p>
+				<h4><span>Về</span> Chúng Tôi</h4>
+				<p><?php echo $about['description'] ?></p>
 			</div>
 		</div>
 	</div>
